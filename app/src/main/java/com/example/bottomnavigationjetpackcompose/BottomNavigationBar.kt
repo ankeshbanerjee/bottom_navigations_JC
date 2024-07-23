c0ba23ac4b3a87bea1ec47ddc95d03f6b7ac5eb5
ankeshbanerjee <banerjeeankesh@gmail.com>
2024-07-23 20:41:43 +0530

    removed unnecessary files

--- app/src/main/java/com/example/bottomnavigationjetpackcompose/BottomNavigationBar.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bottomnavigationjetpackcompose
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-
-@Composable
-fun BottomBar(navController: NavHostController) {
-
-}
